f750fdb4a9149d19a11e5722bb5ea097064d612d
PetarM09 <33965172+PetarM09@users.noreply.github.com>
2026-05-06 13:52:33 +0200

    Bootstrap projekta: Spring Boot 4.0.6, pom.xml sa svim dependencijama, application.yml, Docker Compose za PostgreSQL/pgAdmin/Mailpit

---
+++ src/main/java/rs/skola/platforma/SkolskaPlatformaApplication.java
@@ -0,0 +1,13 @@
+package rs.skola.platforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkolskaPlatformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkolskaPlatformaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/skola/platforma/SkolskaPlatformaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.skola.platforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkolskaPlatformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
